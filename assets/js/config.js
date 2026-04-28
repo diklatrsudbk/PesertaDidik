5035b04cc3490d3adead12a780b4938173b508d6
diklatrsudbk <diklatrsudbk2025@gmail.com>
2026-04-29 06:37:03 +0700

    Add max size limits to config

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,6 @@
 const CONFIG = {
-  SCRIPT_URL: "https://script.google.com/macros/s/AKfycby2878XyWx79MpBjwQSR137ocsaYIz_IwTSgzJruTu1Ls-X1VIG6hvG4ooPVPteukLUVg/exec",
-  TARGET_SHEET: "GEL6"
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycby2878XyWx79MpBjwQSR137ocsaYIz_IwTSgzJruTu1Ls-X1VIG6hvG4ooPVPteukLUVg/exec",
+    TARGET_SHEET: "GEL6",
+    MAX_PHOTO_SIZE: 400 * 1024,
+    MAX_DOC_SIZE: 4 * 1024 * 1024
 };
