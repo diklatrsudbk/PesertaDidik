fb5205a54598a512b5c2d97b7c2b4ade1068412a
diklatrsudbk <diklatrsudbk2025@gmail.com>
2026-04-29 06:16:33 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,9 +1,4 @@
 const CONFIG = {
-
-  API_URL: "https://script.google.com/macros/s/AKfycby2878XyWx79MpBjwQSR137ocsaYIz_IwTSgzJruTu1Ls-X1VIG6hvG4ooPVPteukLUVg/exec",
-
-  MAX_PHOTO_SIZE: 400 * 1024,
-
-  MAX_TOTAL_PDF: 3 * 1024 * 1024
-
+  SCRIPT_URL: "https://script.google.com/macros/s/AKfycby2878XyWx79MpBjwQSR137ocsaYIz_IwTSgzJruTu1Ls-X1VIG6hvG4ooPVPteukLUVg/exec",
+  TARGET_SHEET: "GEL6"
 };
